1bc2244f29d15122996f5d30a0a9a53e0f980e1a
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-10-13 16:10:01 +0700

    feat: main run all part assembler -> simulator

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 Assignment Information: http://myweb.cmu.ac.th/sansanee.a/ComputerArchitecture/Y2022/Project1.htm
 
-Examples on how to use assembler:
+How to run:
+This will compile the assembly to machine code and save the compiled file then show the result from simulator.
+`python .\main.py .\programs\example.s`
+
+If you want to run on a specific part:
+
+- Examples on how to use assembler:
 `python .\assembler.py .\tests\files\t2.s`
+
+- Examples on how to use simulator:
+`python .\simulator.py .\tests\files\test_sim_example.bin`
---
+++ main.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import sys
+from assembler.asm_parser import Parser
+from simulator.simulator import Simulator
+
+def main():
+    arg_parser = argparse.ArgumentParser(description='')
+    arg_parser.add_argument('path', metavar='path', type=str, help='the path to assembly *.s file')
+
+    args = arg_parser.parse_args()
+    path = args.path
+
+    if not os.path.exists(path):
+        print('The path specified does not exist')
+        sys.exit()
+    
+    compiled_path = path.split('\\')
+    compiled_path = '.\programs\compiled\\' + compiled_path[-1].replace('.s','.bin')
+    # assembly to machine code
+    with open(path, 'r') as f, open(compiled_path, 'w') as f_compiled:
+        lines = f.read().splitlines()
+        p = Parser(lines).parse()
+        for item in p.execute():
+            if item != p.execute()[-1]:
+                f_compiled.write(str(item)+'\n')
+            else:
+                f_compiled.write(str(item))
+    f.close()
+    f_compiled.close()
+    # run machine code with simulator
+    com = Simulator()
+    com.run_sim(compiled_path)
+    return
+
+if __name__ == '__main__':
+    main()
